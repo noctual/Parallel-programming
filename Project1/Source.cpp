119160183b918b86f3b651494074ed3722965e97
Sychev Ivan <60885752+noctual@users.noreply.github.com>
2021-09-16 10:34:29 +0500

    feat: task 1

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,6 +3,20 @@
 
 int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
+
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	if (world_rank == 0) {
+		int send_number = 7;
+		MPI_Send(&send_number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	}
+	else if (world_rank == 1) {
+		int recv_number = NULL;
+		MPI_Recv(&recv_number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Process 1 received number %d from process 0\n", recv_number);
+	}
+
 	MPI_Finalize();
 	return 0;
 }
