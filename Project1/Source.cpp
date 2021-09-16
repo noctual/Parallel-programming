275fc8476e20ee933645d0d8da88dfb3fe2e6232
Sychev Ivan <60885752+noctual@users.noreply.github.com>
2021-09-16 10:30:22 +0500

    feat: test task

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,7 +2,10 @@
 #include "mpi.h"
 
 int main(int argc, char* argv[]) {
+	int size;
 	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	printf("%d", &size);
 	MPI_Finalize();
 	return 0;
 }
